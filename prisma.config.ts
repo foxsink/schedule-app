12ac874aacbe096760a6b1be8bc079cc7d418a2c
Yaroslav Karpov <iar.karpov@gmail.com>
2026-02-26 00:19:38 +0700

    chore: update database connection URL in Prisma configuration
    - Replaced the hardcoded database URL with an environment variable (DATABASE_URL) for improved security and flexibility in configuration.

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'prisma/config';
-import { config } from './src/config';
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: config.databaseUrl,
+    url: process.env.DATABASE_URL,
   },
 });
